733fb19f46b8828f3315bb5184f39dc5b1b4f33b
pengbo43 <pengbo43@jd.com>
2022-08-30 13:06:41 +0800

    feat: init

---
+++ src/logEnvPlugin.ts
@@ -0,0 +1,30 @@
+import { ViteDevServer, PluginOption } from 'vite';
+import colors from 'picocolors'
+
+export interface PluginConfig {
+  envKey?: string,
+  strGetter?: () => string
+}
+
+export const logEnvPlugin = (configParams: PluginConfig = {}): PluginOption => {
+  const { envKey = 'NODE_ENV', strGetter } = configParams
+
+  const formateLogStr = `current runing env: ${strGetter ? strGetter() : process.env[envKey]}`
+
+  return {
+    enforce: 'post',
+    name: 'vite:log-env',
+    configureServer(server: ViteDevServer) {
+      const { config } = server
+      return () => {
+        server.middlewares.use((req, res, next) => {
+          next()
+          config.logger.info(
+            colors.green(formateLogStr),
+            { clear: false, timestamp: true }
+          )
+        })
+      }
+    },
+  }
+}
