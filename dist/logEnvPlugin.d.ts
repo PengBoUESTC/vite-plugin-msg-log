15475577756f276756261e9e7dd407c2c752fa0d
pengbo43 <pengbo43@jd.com>
2022-08-30 13:29:37 +0800

    feat: no build

--- dist/logEnvPlugin.d.ts
+++
@@ -1,6 +0,0 @@
-import { PluginOption } from 'vite';
-export interface PluginConfig {
-    envKey?: string;
-    strGetter?: () => string;
-}
-export declare const logEnvPlugin: (configParams?: PluginConfig) => PluginOption;
