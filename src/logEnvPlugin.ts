50339085331620b19bac88a18298c38724b92872
pengbo43 <pengbo43@jd.com>
2022-09-04 10:27:43 +0800

    chore: lint version

--- src/logEnvPlugin.ts
+++ src/logEnvPlugin.ts
@@ -1,36 +1,37 @@
 import { ViteDevServer, PluginOption } from 'vite';
-import { green } from 'picocolors'
+import { green } from 'picocolors';
 
 export interface PluginConfig {
-  envKey?: string,
-  strGetter?: () => string
+  envKey?: string;
+  strGetter?: () => string;
 }
 
 export const logEnvPlugin = (configParams: PluginConfig = {}): PluginOption => {
-  const { envKey = 'NODE_ENV', strGetter } = configParams
+  const { envKey = 'NODE_ENV', strGetter } = configParams;
 
-  const formateLogStr = () => strGetter ? strGetter() : `current runing env: ${process.env[envKey]}`
+  const formateLogStr = () =>
+    strGetter ? strGetter() : `current runing env: ${process.env[envKey]}`;
   const logger = (server: ViteDevServer) => {
-    const { config } = server
+    const { config } = server;
 
-    config.logger.info(
-      green(formateLogStr()),
-      { clear: false, timestamp: true }
-    )
-  }
+    config.logger.info(green(formateLogStr()), {
+      clear: false,
+      timestamp: true,
+    });
+  };
   return {
     enforce: 'post',
     name: 'vite:log-env',
     configureServer(server: ViteDevServer) {
       return () => {
         server.middlewares.use((req, res, next) => {
-          next()
-          logger(server)
-        })
-      }
+          next();
+          logger(server);
+        });
+      };
     },
     handleHotUpdate({ server }) {
-      logger(server)
-    }
-  }
-}
+      logger(server);
+    },
+  };
+};
